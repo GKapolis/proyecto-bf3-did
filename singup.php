3550dbf1d4dc261b319679ab2fee012c7e6a3c7c
GKapolis <74798411+GKapolis@users.noreply.github.com>
2021-10-25 03:19:57 -0300

    Update singup.php

--- singup.php
+++ singup.php
@@ -7,6 +7,7 @@ require_once 'include/errorhandling.php';
                 <h2 class="text-info">aqui podes registrarte</h2>
                 <form action="include/singup.inc.php" method="post">
                     <input type="text" name="username" placeholder="inserte nombre de usuario">
+                    <input type="text" name="name" placeholder="inserte su nombre">
                     <input type="text" name="email" placeholder="inserte correo">
                     <input type="password" name="contraseña" placeholder="inserte contraseña">
                     <input type="password" name="contraseña-repeat" placeholder="reinserte contraseña">
